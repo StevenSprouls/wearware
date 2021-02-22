3e5e4834b68527f16883ba24b831d37953b6e2bc
Yen Clutter <>
2021-02-21 19:44:09 -0700

    added a landing page and navbar

--- wearwareAPI/wearware/WearWareRESTAPI/urls.py
+++ wearwareAPI/wearware/WearWareRESTAPI/urls.py
@@ -1,31 +1,33 @@
 from django.conf.urls import include, url
 from WearWareRESTAPI import views
+from django.urls import path
 
-
+app_name= 'WearWareRESTAPI'
 urlpatterns = [
+  path("", views.index, name="index"),
 
   url(r'^activitylevel/(?P<id>[0-9]+)/$', views.ActivityLevelAPIView.as_view()),
-  url(r'^activitylevel/$', views.ActivityLevelAPIListView.as_view()),
+  url(r'^activitylevel/$', views.ActivityLevelAPIListView.as_view(), name='activitylevel'),
 
   url(r'^heartrate/(?P<id>[0-9]+)/$', views.HeartRateAPIView.as_view()),
-  url(r'^heartrate/$', views.HeartRateAPIListView.as_view()),
+  url(r'^heartrate/$', views.HeartRateAPIListView.as_view(), name='heartrate'),
 
   url(r'^participant/(?P<id>[0-9]+)/$', views.ParticipantAPIView.as_view()),
-  url(r'^participant/$', views.ParticipantAPIListView.as_view()),
+  url(r'^participant/$', views.ParticipantAPIListView.as_view(), name='participant'),
 
   url(r'^participantstudy/(?P<id>[0-9]+)/$', views.ParticipantStudyAPIView.as_view()),
-  url(r'^participantstudy/$', views.ParticipantStudyAPIListView.as_view()),
+  url(r'^participantstudy/$', views.ParticipantStudyAPIListView.as_view(), name='participantstudy'),
 
   url(r'^researcher/(?P<id>[0-9]+)/$', views.ResearcherAPIView.as_view()),
-  url(r'^researcher/$', views.ResearcherAPIListView.as_view()),
+  url(r'^researcher/$', views.ResearcherAPIListView.as_view(), name='researcher'),
 
   url(r'^researcherstudy/(?P<id>[0-9]+)/$', views.ResearcherStudyAPIView.as_view()),
-  url(r'^researcherstudy/$', views.ResearcherStudyAPIListView.as_view()),
+  url(r'^researcherstudy/$', views.ResearcherStudyAPIListView.as_view(), name='researcherstudy'),
 
   url(r'^sleepdata/(?P<id>[0-9]+)/$', views.SleepDataAPIView.as_view()),
-  url(r'^sleepdata/$', views.SleepDataAPIListView.as_view()),
+  url(r'^sleepdata/$', views.SleepDataAPIListView.as_view(), name='sleepdata'),
 
   url(r'^study/(?P<id>[0-9]+)/$', views.StudyAPIView.as_view()),
-  url(r'^study/$', views.StudyAPIListView.as_view()),
+  url(r'^study/$', views.StudyAPIListView.as_view(), name='study'),
 
 ]
--- wearwareAPI/wearware/wearwareAPI/urls.py
+++ wearwareAPI/wearware/wearwareAPI/urls.py
@@ -1,31 +1,10 @@
 from django.conf.urls import include, url
-from WearWareRESTAPI import views
-
+from django.urls import path
+from WearWareRESTAPI import views,urls
 
 urlpatterns = [
 
-  url(r'^activitylevel/(?P<id>[0-9]+)/$', views.ActivityLevelAPIView.as_view()),
-  url(r'^activitylevel/$', views.ActivityLevelAPIListView.as_view()),
+  path("", views.index, name="index"),
 
-  url(r'^heartrate/(?P<id>[0-9]+)/$', views.HeartRateAPIView.as_view()),
-  url(r'^heartrate/$', views.HeartRateAPIListView.as_view()),
-
-  url(r'^participant/(?P<id>[0-9]+)/$', views.ParticipantAPIView.as_view()),
-  url(r'^participant/$', views.ParticipantAPIListView.as_view()),
-
-  url(r'^participantstudy/(?P<id>[0-9]+)/$', views.ParticipantStudyAPIView.as_view()),
-  url(r'^participantstudy/$', views.ParticipantStudyAPIListView.as_view()),
-
-  url(r'^researcher/(?P<id>[0-9]+)/$', views.ResearcherAPIView.as_view()),
-  url(r'^researcher/$', views.ResearcherAPIListView.as_view()),
-
-  url(r'^researcherstudy/(?P<id>[0-9]+)/$', views.ResearcherStudyAPIView.as_view()),
-  url(r'^researcherstudy/$', views.ResearcherStudyAPIListView.as_view()),
-
-  url(r'^sleepdata/(?P<id>[0-9]+)/$', views.SleepDataAPIView.as_view()),
-  url(r'^sleepdata/$', views.SleepDataAPIListView.as_view()),
-
-  url(r'^study/(?P<id>[0-9]+)/$', views.StudyAPIView.as_view()),
-  url(r'^study/$', views.StudyAPIListView.as_view()),
-
+  path('WearWareRESTAPI', include('WearWareRESTAPI.urls')),
 ]
