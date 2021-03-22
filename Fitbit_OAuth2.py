8237647518630a1e298cce023e44a5eb37e41b80
jensenroeNAU <78444653+jensenroeNAU@users.noreply.github.com>
2021-03-22 15:09:57 -0700

    Email participants + participant db entry

--- Fitbit_OAuth2.py
+++ Fitbit_OAuth2.py
@@ -8,6 +8,9 @@ import json
 import sqlalchemy
 from sqlalchemy import create_engine, insert
 import psycopg2
+from flask_mail import Mail,Message
+
+
 
 client_id = '22C4KD'
 client_secret = 'd25cd8564b744d78b92b920e074bb555'
@@ -18,12 +21,28 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'wearableinformaticstest2@gmail.com'
+app.config['MAIL_PASSWORD'] = 'B9UUeTfgr2FBFrv'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
 #index landing page, will be changed upon migration to Amazon EC2
 @app.route('/')
-def hello():
-    fitbit = OAuth2Session(client_id)
+def index():
+    #fitbit = OAuth2Session(client_id)
     authorization_url = fitbit_build_auth_url()
-    return redirect(authorization_url)
+    mail = Mail(app)
+    msg = Message("You have been invited to participate in a study!",
+                  sender = "wearableinformaticstest@gmail.com",
+                  recipients = ["jensenroe@gmail.com"])
+    msg.body = "Hello Jensen,\nYou have been invited to participate in a study using the following link:"+authorization_url
+    mail.send(msg)
+                  
+                  
+                  
+    return ("Hello world this is a test")
 
 
 #callback url, will be changed upon migration to Amazon EC2
@@ -37,24 +56,29 @@ def callback():
     response = fitbit.get('https://api.fitbit.com/1/user/-/profile.json')
     response = json.loads(response.text)
     user_timezone = response['user']['timezone']
+    user_fullname = response['user']['fullName']
+    user_fullname = user_fullname.split(' ')
+    
 
     
     #connecting to our database and setting access_token + refresh_token
     #plus basic user profile information
     engine = create_engine(DATABASE_URI)
-    sql_command = 'INSERT INTO public.\"WearWareRESTAPI_fitbitaccount\" VALUES (DEFAULT,9998887,True,\''+user_timezone+'\',\'auth_token\',\''+str(session['oauth_token']['refresh_token'])+'\',\''+str(session['oauth_token']['refresh_token'])+'\','+str(859)+')'
+    sql_command_participant = 'INSERT INTO public.\"WearWareRESTAPI_participant\" VALUES (1338,\''+user_fullname[0]+'\',\''+user_fullname[1]+'\',\'jensenroe@gmail.com\',\'M\',\'M\',\'40e6215d-b5c6-4896-987c-f30f3678f608\',True)'
+
+    engine.execute(sql_command_participant)
+
+    sql_command_fitbitaccount = 'INSERT INTO public.\"WearWareRESTAPI_fitbitaccount\" VALUES (DEFAULT,9998887,True,\''+user_timezone+'\',\'auth_token\',\''+str(session['oauth_token']['refresh_token'])+'\',\''+str(session['oauth_token']['refresh_token'])+'\','+str(1338)+')'
                    
-    engine.execute(sql_command)
+    engine.execute(sql_command_fitbitaccount)
     
 
-    return redirect(url_for('.activity'))
+    return redirect(url_for('.success'))
 
 #
-@app.route('/activity', methods=["GET"])
-def activity():
-    fitbit = OAuth2Session(client_id, token=session['oauth_token'])
-    response = fitbit.get('https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json').json()
-    return json.dumps(response)
+@app.route('/success')
+def success():
+    return ("You have successfully authorized your fitbit account with the wearware application.\n\n You may close out of this window now.")
 
 #Construct an authentication URL for a given subject.
 def fitbit_build_auth_url():
